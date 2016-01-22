b2c5f144fcb13ae6a5f35678d8048e4fb53be38a
Chris Johnson <johnsonchris98@gmail.com>
2016-01-21 19:27:12 -0500

    working

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = "mongodb://localhost:27017/surfApp";
+var db;
+
+
+MongoClient.connect(mongoUrl, function(err, database){
+  if (err) {
+    console.log(err);
+  }
+  console.log("connected!");
+  db = database;
+  process.on('exit', db.close);
+})
+
+
 // msw.set({ apiKey: process.env.SEAWEED_KEY, units: 'us' });
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -37,17 +53,39 @@ app.get('/', function(req, res){
 		// console.log(swellDirection)
 		res.render('index', {results: body})
 	})
+
+	
+
 });
 
-app.get('/report/1', function(req, res){
-	request('http://magicseaweed.com/api/'+process.env.SEAWEED_KEY+'/forecast/?spot_id=10', function(err, results){
+app.get('/report/:id', function(req, res){
+	var locationId = req.params.id;
+	console.log('LOCATION ID ', locationId);
+	request('http://magicseaweed.com/api/'+process.env.SEAWEED_KEY+'/forecast/?spot_id=' + locationId, function(err, results){
 		if (err) {
 			console.log("error")
 		}
-		body = JSON.parse(results.body)
+		var body = JSON.parse(results.body);
+		console.log('BODY ', body);
 		swellDirection = Math.ceil(body[0].swell.components.combined.direction/5)*5
 		res.render('report', {results: body})
-	})
+	});
+	request('https://api.instagram.com/v1/tags/rockawaybeach/media/recent?access_token='+process.env.INSTAGRAM_KEY, function(err, results){
+		if (err) {
+			console.log("error")
+		}
+		var instagramData = JSON.parse(results.body);
+		console.log('BODY ', instagramData);
+		res.render('report', {results: instagramData})
+	});
+});
+
+app.get('/api/locations', function(req, res) {
+	console.log('HIT API LOCATIONS');
+	db.collection('locations').find({}).toArray(function(err, result) {
+ 		res.send(result) 
+ 	})
+	// res.end();
 });
 
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,8 +3,27 @@ console.log("linked")
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    var selectLocations;
+
+    function _selectOptions(data) {
+        selectLocations = $('#select-locations');
+        data.forEach(function(element, index) {
+            var option = $('<option></option>');
+            option.attr('value', element.id);
+            option.text(element.name);
+            selectLocations.append(option);
+        });
+    }
+
     $('#entry').click(function() {
-      location.href='/report/1'
-    })
+      var locationId = selectLocations.val();
+      console.log('LOCATION ID ', locationId);
+      location.href = '/report/' + locationId;
+    });
+
+    $.get('/api/locations').done(function(data) {
+        console.log('SUCCESS DATA ', data);
+        _selectOptions(data);
+    });
 
 });
