a08c5643202844d1e33f1bec2340acb3c5e5e0ce
Chris Johnson <johnsonchris98@gmail.com>
2016-01-25 18:06:05 -0500

    deploy

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ MongoClient.connect(mongoUrl, function(err, database){
 })
 
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
